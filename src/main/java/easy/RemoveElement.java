ba035bebc8b88f9b2dfa8d714aa961d200ba31f7
yo1o1o633o <296077244@qq.com>
2020-04-18 11:14:33 +0800

    删除排序数组中的重复项

--- src/main/java/easy/RemoveElement.java
+++ src/main/java/easy/RemoveElement.java
@@ -13,16 +13,21 @@ public class RemoveElement {
      *
      * */
     public static void main(String[] args) {
+        /**
+         * 引用传递, 所以定义两个数组测试, 否则第一个方法会影响第二个方法的结果. 如果传入同一个数组的话
+         * */
         int[] num = new int[]{3,2,2,3};
-        RemoveElement removeElement = new RemoveElement();
-        removeElement.removeElement(num, 3);
+        int[] num2 = new int[]{3,2,2,3};
+        int val = 3;
+        System.out.println(removeElement(num, val));
+        System.out.println(removeElement2(num2, val));
     }
 
     /**
-     * 此解法本地执行有问题, 力扣正常
-     * 本地输出2223
+     * 双指针
+     * 对题解理解有问题. 只是将后一个元素向前复制. 数组的大小并没有变. 是复制不是移动. 最后截取满足条件个数的数组元素
      * */
-    private int removeElement(int[] nums, int val) {
+    private static int removeElement(int[] nums, int val) {
         int j = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
@@ -30,7 +35,25 @@ public class RemoveElement {
                 j++;
             }
         }
-        System.out.println(nums.length);
         return j;
+    }
+
+    /**
+     * 双指针, 循环判断, 如果相等就将最后一个元素复制到当前的元素位置.
+     * 如果不相等, 那么向后移动继续判断
+     * 每发现一个相等的元素, 长度就减一
+     * */
+    private static int removeElement2(int[] nums, int val) {
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                n--;
+            } else {
+                i++;
+            }
+        }
+        return n;
     }
 }
