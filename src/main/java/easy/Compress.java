a880599161332c270ba6ea13d3a09b7166bb2caf
shuai.yang <shuai.yang@corp.to8to.com>
2020-04-26 20:19:39 +0800

    压缩字符串

--- src/main/java/easy/Compress.java
+++ src/main/java/easy/Compress.java
@@ -5,6 +5,15 @@ package easy;
  */
 
 public class Compress {
+    /**
+     * 给定一组字符，使用原地算法将其压缩。
+     *
+     * 压缩后的长度必须始终小于或等于原数组长度。
+     *
+     * 数组的每个元素应该是长度为1 的字符（不是 int 整数类型）。
+     *
+     * 在完成原地修改输入数组后，返回数组的新长度。
+     * */
     public static void main(String[] args) {
         char[] chars = {'a','a','b','b','b','b','b','b','b','b','b','b','b','b','d'};
         System.out.println(compress(chars));
