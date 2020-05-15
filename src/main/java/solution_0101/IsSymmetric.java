6f37d04d5e84aa8706501f133518284118830739
shuai.yang <shuai.yang@corp.to8to.com>
2020-05-15 16:36:37 +0800

    对称二叉树

--- src/main/java/solution_0101/IsSymmetric.java
+++ src/main/java/solution_0101/IsSymmetric.java
@@ -1,5 +1,8 @@
 package solution_0101;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author shuai.yang
  */
@@ -41,5 +44,37 @@ public class IsSymmetric {
             return false;
         }
         return left.val == right.val && checkout(left.left, right.right) && checkout(left.right, right.left);
+    }
+
+    /**
+     * 迭代
+     * 每次将左的左和右的右放入队列. 左的右和右的左再放入队列
+     * 每次取出两个进行迭代判断
+     * 其实这里的先遍历了树的左的左的左的左...  右的右的右的右
+     * 当上边的满足后
+     * 再遍历左的右 右的左. 即中间的
+     * */
+    private static boolean isSymmetric2(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode t1 = queue.poll();
+            TreeNode t2 = queue.poll();
+            if (t1 == null && t2 == null) {
+                continue;
+            }
+            if (t1 == null || t2 == null) {
+                return false;
+            }
+            if (t1.val != t2.val) {
+                return false;
+            }
+            queue.add(t1.left);
+            queue.add(t2.right);
+            queue.add(t1.right);
+            queue.add(t2.left);
+        }
+        return true;
     }
 }
