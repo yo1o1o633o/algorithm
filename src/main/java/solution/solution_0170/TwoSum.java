d63f83abe223487350d4338219622ed72377b090
shuai.yang <shuai.yang@corp.to8to.com>
2020-06-01 14:38:19 +0800

    两数之和 III - 数据结构设计

---
+++ src/main/java/solution/solution_0170/TwoSum.java
@@ -0,0 +1,40 @@
+package solution.solution_0170;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author shuai.yang
+ */
+public class TwoSum {
+    /**
+     * 用hashMap来存储添加得元素.
+     * */
+    private Map<Integer, Integer> nums = null;
+
+    public TwoSum() {
+        nums = new HashMap<>();
+    }
+
+    public void add(int number) {
+        nums.put(number, nums.getOrDefault(number, 0) + 1);
+    }
+
+    public boolean find(int value) {
+        for (Map.Entry<Integer, Integer> num : nums.entrySet()) {
+            Integer res = value - num.getKey();
+            if (!res.equals(num.getKey())) {
+                if (nums.containsKey(res)) {
+                    return true;
+                }
+            } else {
+                if (num.getValue() > 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
