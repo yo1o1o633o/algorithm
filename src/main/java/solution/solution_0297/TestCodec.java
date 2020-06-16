eb9d87a83736dd93dadbac070bbe40e9541d9772
shuai.yang <shuai.yang@corp.to8to.com>
2020-06-16 19:09:12 +0800

    二叉树的序列化与反序列化

---
+++ src/main/java/solution/solution_0297/TestCodec.java
@@ -0,0 +1,25 @@
+package solution.solution_0297;
+
+import entity.TreeNode;
+
+/**
+ * @author shuai.yang
+ */
+public class TestCodec {
+    /**
+     * 序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，同时也可以通过网络传输到另一个计算机环境，采取相反方式重构得到原数据。
+     *
+     * 请设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
+     * */
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(1);
+        treeNode.left = new TreeNode(2);
+        treeNode.right = new TreeNode(3);
+        treeNode.right.left = new TreeNode(4);
+        treeNode.right.right = new TreeNode(5);
+        Codec codec = new Codec();
+        String s = codec.serialize(treeNode);
+        System.out.println(s);
+        codec.deserialize(s);
+    }
+}
