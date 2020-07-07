1e56f185b4d55dc056321cc5e51c365851e34fbe
yangs <296077244@qq.com>
2020-07-07 23:43:13 +0800

    二叉树的右视图

---
+++ src/main/java/solution/solution_0199/RightSideView.java
@@ -0,0 +1,53 @@
+package solution.solution_0199;
+
+import entity.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class RightSideView {
+    /**
+     * 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+     * */
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(1);
+        treeNode.left = new TreeNode(2);
+        treeNode.left.right = new TreeNode(5);
+        treeNode.right = new TreeNode(3);
+        treeNode.right.right = new TreeNode(4);
+        System.out.println(rightSideView(treeNode));
+    }
+
+    /**
+     * 广度优先搜索
+     * */
+    private static List<Integer> rightSideView(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        List<Integer> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                if (poll != null) {
+                    // 将当前层的最后一个节点的值放入结果中
+                    if (i == size - 1) {
+                        result.add(poll.val);
+                    }
+                    if (poll.left != null) {
+                        queue.add(poll.left);
+                    }
+                    if (poll.right != null) {
+                        queue.add(poll.right);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
