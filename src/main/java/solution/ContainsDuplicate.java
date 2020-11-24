f489bccdc9ada8b50089c2e1bc24de24325a4ae2
shuai.yang <shuai.yang@corp.to8to.com>
2020-11-24 20:20:48 +0800

    存在重复元素,删除排序数组中的重复项

---
+++ src/main/java/solution/ContainsDuplicate.java
@@ -0,0 +1,59 @@
+package solution;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 存在重复元素
+ * 给定一个整数数组，判断是否存在重复元素。
+ *
+ * 如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+ *
+ * @author shuai.yang
+ */
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,3,3,4,3,2,4,2};
+        System.out.println(containsDuplicateWithHash(nums));
+        System.out.println(containsDuplicateWithDoublePoint(nums));
+    }
+
+    /**
+     * 哈希表. 如果Map内存在, 则证明遇到重复元素, 返回true
+     * 否则给Map添加元素1
+     * */
+    private static boolean containsDuplicateWithHash(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>(16);
+        for (int n : nums) {
+            if (map.containsKey(n)) {
+                return true;
+            }
+            map.put(n, 1);
+        }
+        return false;
+    }
+
+    /**
+     * 双指针
+     * 利用排序方法对数组进行排序
+     * 如果有重复的那么一定是挨着的
+     * 双指针两两比较. 碰到相同的返回true
+     * */
+    private static boolean containsDuplicateWithDoublePoint(int[] nums) {
+        if (nums.length <= 1) {
+            return false;
+        }
+        Arrays.sort(nums);
+        int i = 0;
+        int j = i + 1;
+        while (j < nums.length) {
+            if (nums[i] == nums[j]) {
+                return true;
+            }
+            i++;
+            j++;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/solution/RemoveDuplicates.java
@@ -0,0 +1,43 @@
+package solution;
+
+/**
+ * 删除排序数组中的重复项
+ * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ *
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ *
+ * @author shuai.yang
+ */
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(nums));
+    }
+
+    /**
+     * 双指针.
+     * 快指针用来寻找下一个比慢指针大的值, 赋值给慢指针的下一个元素.
+     * 每次替换计数值+1
+     * 最后统计总的计数值. i是从0开始的, 移动的次数+1就是结果数组
+     * */
+    private static int removeDuplicates(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        int i = 0;
+        int j = 0;
+        while (j < nums.length) {
+            // 如果相同,快指针前进
+            if (nums[i] == nums[j]) {
+                j++;
+                continue;
+            }
+            // 如果大于慢指针, 那么赋值到慢指针下一个元素
+            if (nums[i] < nums[j]) {
+                nums[i + 1] = nums[j];
+                i++;
+            }
+        }
+        return i + 1;
+    }
+}
