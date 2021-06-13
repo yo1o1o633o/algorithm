7ae815215a60b3297b8d9f8a760288fd561e80a9
yangs <296077244@qq.com>
2021-06-13 23:00:43 +0800

    剑指 Offer 05. 替换空格

---
+++ src/main/java/offer/offer_005/Solution.java
@@ -0,0 +1,29 @@
+package offer.offer_005;
+
+/**
+ * 剑指 Offer 05. 替换空格
+ * 
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ * */
+public class Solution {
+    public static void main(String[] args) {
+        String s = "We are happy.";
+        System.out.println(replaceSpace(s));
+    }
+
+    private static String replaceSpaceWithFor(String s) {
+        StringBuilder r = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
+                r.append("%20");
+            } else {
+                r.append(c);
+            }
+        }
+        return r.toString();
+    }
+
+    private static String replaceSpace(String s) {
+        return s.replaceAll(" ", "%20");
+    }
+}
